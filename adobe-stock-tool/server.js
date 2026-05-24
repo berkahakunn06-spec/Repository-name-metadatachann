1a9d1724a9b6336a1452e687f7c0d44d4b8ac6cc
metadatachann <berkahakunn06@gmail.com>
2026-05-24 19:36:09 +0700

    fix railway start

--- adobe-stock-tool/server.js
+++ adobe-stock-tool/server.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("RUNNING PORT " + PORT);
 });
